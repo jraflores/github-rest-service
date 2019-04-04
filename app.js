0c8a41099f410dba91d1c4eec1e2a6b0f7b4bcbb
jraflores <johnraflores@gmail.com>
2019-04-04 11:25:09 -0500

    added cors package

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
+var cors = require('cors');
 var config = require('./config')();
-
 
 // Globals ---------------------------------------------------------------------
 var PORT = config.port;
@@ -9,6 +9,21 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+var allowed = ['http://localhost:3000'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests or Fiddler)
+    if(!origin) return callback(null, true);
+
+    // allow from specific URLs
+    if(allowed.indexOf(origin) === -1){
+      var msg = 'Opps! CORS policy violation!';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Logging ---------------------------------------------------------------------
 var winston = require('winston');
